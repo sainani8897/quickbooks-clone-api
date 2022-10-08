1c7ffbf24e4276a11f69775a419da121bf087c5c
sainani8897 <sainani8897@gmail.com>
2022-10-09 00:34:17 +0530

    Product realation in packages

--- src/controllers/package.controller.js
+++ src/controllers/package.controller.js
@@ -9,7 +9,7 @@ exports.index = async function (req, res, next) {
       page: req.query.page ?? 1,
       limit: req.query.limit ?? 10,
       sort: { createdAt: -1 },
-      populate: ['sales_order']
+      populate: ['sales_order','package.product_id']
     };
 
     const query = req.query;
