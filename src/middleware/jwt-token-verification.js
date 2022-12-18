3ed78c933b5fe313037b2c3fa71801993ffe1592
sainani8897 <sainani8897@gitlab.com>
2022-12-18 10:28:08 +0530

    organization filter added

--- src/middleware/jwt-token-verification.js
+++ src/middleware/jwt-token-verification.js
@@ -23,6 +23,7 @@ exports.authenticateToken = async (req, res, next) => {
     if (!user_data) throw new UnauthorizedException("Unauthoirzed");
     req.user = user_data.user;
     req.token = user_data.token;
+    req.query.org_id = user_data.org_id
     next();
   } catch (error) {
     next(error);
